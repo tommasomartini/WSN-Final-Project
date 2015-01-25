c092bf58e1f2acf1456025fa0101466df156ff9b
Tommaso <tommaso.martini91@gmail.com>
2015-01-25 09:54:43 +0100

    restred storage node message

--- src/project_2.cpp
+++ src/project_2.cpp
@@ -1,6 +1,6 @@
 /*
 
-g++ project_2.cpp event.cpp node.cpp measure.cpp my_toolbox.cpp sensor_node.cpp storage_node.cpp blacklist_message.cpp message.cpp node_dispatcher.cpp user.cpp -o wir -std=c++11 
+g++ project_2.cpp event.cpp node.cpp measure.cpp my_toolbox.cpp sensor_node.cpp storage_node.cpp blacklist_message.cpp message.cpp user_message.cpp outdated_measure.cpp user.cpp -o wir -std=c++11 
 
 -pthread -std=c++11
 
